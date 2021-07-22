ae18b965f1e62419992c34f7e2423e856e647fc2
ethriel3695 <ethriel3695@gmail.com>
2021-07-22 01:34:55 -0600

    Fix: update package versions

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -3,19 +3,17 @@ import PropTypes from 'prop-types';
 import { BlogPostTemplate } from '../../components/Post/PostContainer';
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
-  // const tags = entry.getIn(['data', 'tags']);
   const title = entry.getIn(['data', 'title']);
   const date = entry.getIn(['data', 'date']);
   const data = {
     title,
     date,
   };
-  console.log(data);
+
   return (
     <BlogPostTemplate
       content={widgetFor('body')}
       isPreview={true}
-      // tags={tags && tags.toJS()}
       data={data}
     />
   );
